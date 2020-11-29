ffa57b0028c6f8ba3c634fd6715fe9dbd046e8e0
Girish Motwani <girish.motwani@gmail.com>
2020-11-29 01:12:46 -0800

    fix variable naming

--- lib/jammy/armclient.py
+++ lib/jammy/armclient.py
@@ -17,12 +17,12 @@ class ArmClient(object):
     """
 
     def __init__(self):
-        self._baseUrl = 'https://management.azure.com'
+        self._base_url = 'https://management.azure.com'
         self._armclient = 'armclient.exe'
 
     @property
-    def baseUrl(self):
-        return self._baseUrl
+    def base_url(self):
+        return self._base_url
 
     def get_resource_group_body(self, location):
         body = { "location": location}
@@ -45,18 +45,18 @@ class ArmClient(object):
 
 
 
-    def create_resource_group(self, subscriptionId, resource_group_name, location):
-        resource_group = '/subscriptions/' + subscriptionId + '/resourceGroups/' + resource_group_name
+    def create_resource_group(self, subscription_id, resource_group_name, location):
+        resource_group = '/subscriptions/' + subscription_id + '/resourceGroups/' + resource_group_name
         resource_json = self.get_resource_group_body(location)
         self.put_resource(resource_group, resource_json, '2019-10-01')
 
-    def deploy_template(self, subscriptionId, deployment_name, resource_group_name, location, template_file, template_params=''):
-        self.create_resource_group(subscriptionId, resource_group_name, location)
-        resourceId = '/subscriptions/' + subscriptionId + '/resourceGroups/' + resource_group_name + '/providers/Microsoft.Resources/deployments/{0}'.format(deployment_name)
+    def deploy_template(self, subscription_id, deployment_name, resource_group_name, location, template_file, template_params=''):
+        self.create_resource_group(subscription_id, resource_group_name, location)
+        resource_id = '/subscriptions/' + subscription_id + '/resourceGroups/' + resource_group_name + '/providers/Microsoft.Resources/deployments/{0}'.format(deployment_name)
 
         if template_file.startswith('/windir/c'):
             template_file = template_file[len('/windir/c'):]
-        url = self.baseUrl + resourceId + '?api-version=2019-10-01'
+        url = self.base_url + resource_id + '?api-version=2019-10-01'
         headers = ' ' + '-h "Referer: ' + url + '"'
         cmd = self._armclient + " put " + url + " " + "@" + template_file + ' ' + headers
         output = self.cmd_wrapper(cmd)
@@ -69,14 +69,14 @@ class ArmClient(object):
             # incase of failure return string as error
             raise ArmClientError(result)
 
-        self.wait_for_deployment_complete(resourceId, '2019-10-01')
+        self.wait_for_deployment_complete(resource_id, '2019-10-01')
 
         return result 
 
-    def put_resource(self, resourceId, resourceJson, apiVersion):
-        url = self.baseUrl + resourceId + '?api-version=' + apiVersion
+    def put_resource(self, resource_id, resource_json, api_version):
+        url = self.base_url + resource_id + '?api-version=' + api_version
         headers = ' ' + '-h "Referer: ' + url + '"'
-        cmd = self._armclient + " put " + url + " " + "'" +  resourceJson + "'" + headers
+        cmd = self._armclient + " put " + url + " " + "'" +  resource_json + "'" + headers
         output = self.cmd_wrapper(cmd)
 
         result = output.decode("utf-8")
@@ -90,8 +90,8 @@ class ArmClient(object):
         return result 
 
 
-    def get_resource(self, resourceId, apiVersion):
-        url = self.baseUrl + resourceId + '?api-version=' + apiVersion
+    def get_resource(self, resource_id, api_version):
+        url = self.base_url + resource_id + '?api-version=' + api_version
         headers = ' ' + '-h "Referer: ' + url + '"'
         cmd = self._armclient + ' get ' + url + headers
         output = self.cmd_wrapper(cmd)
@@ -106,8 +106,8 @@ class ArmClient(object):
 
         return result
 
-    def delete_resource(self, resourceId, apiVersion):
-        url = self.baseUrl + resourceId + '?api-version=' + apiVersion
+    def delete_resource(self, resource_id, api_version):
+        url = self.base_url + resource_id + '?api-version=' + api_version
         headers = ' ' + '-h "Referer: ' + url + '"'
         cmd = self._armclient + ' delete ' + url + headers
         self.cmd_wrapper(cmd)
